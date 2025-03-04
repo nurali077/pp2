724457df5ad77c44d5ecec2e63eebcf8bba3082b
nurali077 <n_sotsialov@kbtu.kz>
2025-03-04 08:09:08 +0600

    Add file and directory manipulation functions with basic operations

--- Lab6/directories-and-files/1.py
+++ Lab6/directories-and-files/1.py
@@ -1,10 +1,6 @@
 import os
-import shutil
 
-def list_files_and_dirs(path):
-    if not os.path.exists(path):
-        print(f"Ошибка: путь '{path}' не существует!")
-        return
-    print("Directories:", [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))])
-    print("Files:", [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
-    print("All:", os.listdir(path))
+def list_files_and_dirs(path="."):
+    return os.listdir(path)
+
+print(list_files_and_dirs())
